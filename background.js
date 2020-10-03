a16b7366796b264752dc13230aa5867e27ef8b6a
lutangar <johan.dufour@gmail.com>
2020-10-03 21:27:31 +0200

    feat: PoC minimum permission extension `declarativeContent` and `activeTab` Very limited, no automatic popup, no hiding page action, and no badges... You have to click on the page action icon to trigger it.

--- background.js
+++ background.js
@@ -1,44 +1,51 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-const mcBlacklist = ['Moscow', 'moscow'];
-
-
-const createUrlMatchesPageStateMatcher = (urlMatches) => new chrome.declarativeContent.PageStateMatcher({
-  pageUrl: { urlMatches },
-})
-
-// When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function() {
-
-  chrome.pageAction.onClicked.addListener(function(tab) {
-    // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' red!');
+const createUrlMatchesPageStateMatcher = (urlMatches) =>
+  new chrome.declarativeContent.PageStateMatcher({
+    pageUrl: { urlMatches },
   });
 
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, async function() {
-    // With a new rule ...
-    const matchingContexts = await fetch('https://notices.bulles.fr/api/v3/matching-contexts').then(response => response.json())
+const createSetIconAction = (path) =>
+  new Promise((resolve, reject) => {
+    try {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      const image = new Image();
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0, 19, 19);
+        const imageData = ctx.getImageData(0, 0, 19, 19);
+        const action = new chrome.declarativeContent.SetIcon({ imageData });
 
+        return resolve(action);
+      };
+      image.src = chrome.runtime.getURL(path);
+    } catch (e) {
+      reject(e);
+    }
+  });
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, async () => {
+    const matchingContexts = await fetch(
+      'https://notices.bulles.fr/api/v3/matching-contexts'
+    ).then((response) => response.json());
     const showPageAction = new chrome.declarativeContent.ShowPageAction();
-    const rules = Object.values(matchingContexts.filter(mc => !mc.urlRegex.includes('?=')).reduce((rules, mc) => {
-        rules[mc.noticeId.toString()] = {
-          id: mc.noticeId.toString(),
-          ...rules[mc.noticeId] || {},
-          conditions: [
-            ...rules[mc.noticeId.toString()]?.conditions || [],
-            createUrlMatchesPageStateMatcher(mc.urlRegex)
-          ],
-          actions: [showPageAction],
-        }
+    const setIconAction = await createSetIconAction('logo.png');
+    const rules = Object.values(
+      matchingContexts
+        .filter((mc) => !mc.urlRegex.includes('?='))
+        .reduce((rules, mc) => {
+          rules[mc.noticeId.toString()] = {
+            id: mc.noticeId.toString(),
+            ...(rules[mc.noticeId] || {}),
+            conditions: [
+              ...(rules[mc.noticeId.toString()]?.conditions || []),
+              createUrlMatchesPageStateMatcher(mc.urlRegex),
+            ],
+            actions: [showPageAction, setIconAction],
+          };
 
-        return rules;
-    }, {}));
-
-
-    console.log(rules);
-
+          return rules;
+        }, {})
+    );
     chrome.declarativeContent.onPageChanged.addRules(rules);
   });
 });
--- popup.js
+++ popup.js
@@ -1,20 +1,41 @@
-chrome.tabs.query({ active:true, currentWindow:true }, function(tabs) {
-  const currentTabUrl = tabs[0].url;
-  console.log('currentTabUrl', currentTabUrl);
-  chrome.declarativeContent.onPageChanged.getRules(async (rules) => {
-    const noticesIds = rules.filter(
-        ({ conditions }) =>
-            conditions
-                .map(c => c?.pageUrl?.urlMatches)
-                .filter(urlMatches => urlMatches)
-                .some(urlMatches => currentTabUrl.match(urlMatches))
-    ).map(({ id }) => id);
+const render = (content) => {
+  document.getElementById('app').innerHTML = content;
+};
 
-    const notices = await Promise.all(
-        noticesIds
-            .map(noticeId => fetch(`https://notices.bulles.fr/api/v3/notices/${noticeId}`).then(response => response.json()))
-    )
-    console.log(notices);
-    document.write(notices.map(({ message }) => message).join('<br />'));
+const renderNotices = (notices) =>
+  render(
+    notices
+      .map(({ message, contributor: { name } }) => {
+        return `<article>
+        ${message}
+        <em>${name}</em>
+      </article>`;
+      })
+      .join('<hr />')
+  );
+
+const createRuleMatcher = (url) => ({ conditions }) =>
+  conditions
+    .map((c) => c?.pageUrl?.urlMatches)
+    .filter((urlMatches) => urlMatches)
+    .some((urlMatches) => url.match(urlMatches));
+
+try {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const ruleMatcher = createRuleMatcher(tabs[0].url);
+
+    chrome.declarativeContent.onPageChanged.getRules(async (rules) => {
+      const noticesIds = rules.filter(ruleMatcher).map(({ id }) => id);
+      const notices = await Promise.all(
+        noticesIds.map((noticeId) =>
+          fetch(
+            `https://notices.bulles.fr/api/v3/notices/${noticeId}`
+          ).then((response) => response.json())
+        )
+      );
+      renderNotices(notices);
+    });
   });
-});
+} catch (e) {
+  console.error(e);
+}
